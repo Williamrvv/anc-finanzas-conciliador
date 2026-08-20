8ffc911880428eea132c49d246b895741b7704fe
williamrvv <williamrvv@hotmail.com>
2026-08-20 10:47:01 -0600

    trabajando en cambios de guardado de fecha de conciliación v5

--- src/visor_crudos.php
+++ src/visor_crudos.php
@@ -299,7 +299,7 @@ if (preg_match('/^\d{4}-\d{2}-\d{2}$/', $end)) {
                 }
             }
 
-            // 1. Limpiar el buscador visual global
+            // 1. Limpiar el buscador visual global 
             const globalSearch = document.getElementById('global-search');
             if (globalSearch) globalSearch.value = '';
 
